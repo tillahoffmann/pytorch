705d265cbfe81d240e94a518b0a75773577f31aa
Victor Quach <quach@fb.com>
2021-07-09 15:15:44 -0400

    Apply saved tensor hooks
    Summary: Fixes #58512
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 174958711052ab7a1fd2fff2880ccbf7a6c92c5f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/60975

--- torch/csrc/autograd/python_saved_variable_hooks.h
+++ torch/csrc/autograd/python_saved_variable_hooks.h
@@ -14,18 +14,32 @@ struct TORCH_API PySavedVariableHooks : public SavedVariableHooks {
     PySavedVariableHooks(py::function &pack_hook, py::function &unpack_hook) : pack_hook_(pack_hook), unpack_hook_(unpack_hook){}
 
     void call_pack_hook(at::Tensor &tensor) override {
-      TORCH_CHECK_NOT_IMPLEMENTED(false, "Hooks are not implemented yet");
+      py::gil_scoped_acquire acquire;
+      auto wrapped = THPVariable_Wrap(tensor); // here, we own the reference
+      py::object obj = py::reinterpret_steal<py::object>(wrapped); // obj steals the reference
+      py::object packed = pack_hook_(obj); // packed is a new object with a reference
+      data_ = packed.release().ptr(); // steals the reference
+      // obj is decrefed on exit, but we will manually decref data_ when the saved variable is released
     }
 
     at::Tensor call_unpack_hook() override {
-      TORCH_CHECK_NOT_IMPLEMENTED(false, "Hooks are not implemented yet");
+      py::gil_scoped_acquire acquire;
+      py::object obj = py::cast<py::object>(data_); // copies the content of data_
+      py::object res = unpack_hook_(obj); // new object, comes with a reference
+      return THPVariable_Unpack(res.ptr()); // borrows from res, we only need the argument as long as res is alive
+      // obj and res are decrefed on exit
     }
 
-    ~PySavedVariableHooks() override = default;
+    ~PySavedVariableHooks() override {
+      if (data_) {
+        Py_DECREF(data_);
+      }
+    };
 
   private:
     py::function pack_hook_;
     py::function unpack_hook_;
+    PyObject* data_ = nullptr;
 };
 
 }}
